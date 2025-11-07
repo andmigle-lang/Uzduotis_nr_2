b2f4fcbf222e19f6d5cc3b6c60964bafd1150ba1
andmigle-lang <and.migle@gmail.com>
2025-11-07 13:44:27 +0200

    Pridėtas palyginimas klasėms

---
+++ Include/palyginimas_class.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Studentas_klase.h"
+
+bool palyginimas_vardas_class(const Studentas_klase& a, const Studentas_klase& b);
+bool palyginimas_pavarde_class(const Studentas_klase& a, const Studentas_klase& b);
+bool palyginimas_galutinis_class(const Studentas_klase& a, const Studentas_klase& b);
+bool palyginimas_2_strategija_class(const Studentas_klase& a, const Studentas_klase& b);
+bool partition_palyginimas_class(const Studentas_klase& s);
