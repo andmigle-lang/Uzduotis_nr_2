5f1531c5b775c0ec1aa2006c07c3a44421821c3a
andmigle-lang <and.migle@gmail.com>
2025-11-20 11:43:56 +0200

    Update Studentas_klase.h

--- Include/Studentas_klase.h
+++ Include/Studentas_klase.h
@@ -2,8 +2,10 @@
 #include <string>
 #include <vector>
 #include <istream>
+#include <ostream>
 
 using std::istream;
+using std::ostream;
 using std::vector;
 using std::string;
 
@@ -27,11 +29,20 @@ public:
     inline const vector<int>& pazymiai() const { return paz_; }
     inline int egzaminas() const { return egz_; }
 
-    //Nuskaitomas studentas (setteris)
+    //Nuskaitomas studentas (setteris?)
     istream& skaityk_studenta_class(istream& is);
 
     //Suskaiciuojami gal_ ir med_
     void skaiciuok_galutinius();
 
     ~Studentas_klase();
+    //Kopijavimo konstruktorius
+    Studentas_klase(const Studentas_klase& s);
+    //Kopijavimo priskirties operatorius
+    Studentas_klase& operator=(const Studentas_klase& s);
+
+
+    friend std::istream& operator>>(std::istream& in, Studentas_klase& s);
+    friend std::ostream& operator<<(std::ostream& out, const Studentas_klase& s);
 };
+
